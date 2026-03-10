4c55d970e390d730ff9050cf22dde11dd0a16b88
winstone-1 <winstonemuna404@gmail.com>
2026-03-11 00:39:40 +0300

    implement sidebar toggle functionality and update UI elements

--- JS/script.js
+++ JS/script.js
@@ -126,6 +126,16 @@ function renderFrame() {
     }
 }
 
+
+function toggleSidebar() {
+  const sidebar = document.querySelector('aside');
+  const main    = document.querySelector('.ml-64');
+
+  sidebar.classList.toggle('-translate-x-full');
+  main.classList.toggle('ml-0');
+  main.classList.toggle('ml-64');
+   overlay.classList.toggle('hidden');
+}
 // 6. SIDEBAR MENU TOGGLE LOGIC
 const menuButtons = document.querySelectorAll('.menu-btn');
 const mobileMenus = document.querySelectorAll('.mobile-menu');
